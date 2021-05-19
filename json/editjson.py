81d462e8d7397f1f0f91effcd907496f80b08b75
boma25 <amakirisamuelboma@gmail.com>
2021-05-19 22:35:09 +0100

    fixed issues with the write to file

--- json/editjson.py
+++ json/editjson.py
@@ -11,18 +11,16 @@ total = len(data)
 average = round(total/divisor)
 incrimentor = 0
 
+new_file ={}
 for i in range(0,average):
     #print(f"{incrimentor+1} - {divisor*(i+1)}", data[f"{incrimentor}"])
-    new_file ={}
     new_file[f"{incrimentor+1} - {divisor*(i+1)}"] = data[f"{incrimentor}"]
     #new_file = incrimentor+1 - divisor*(i+1), data[incrimentor]
     print(new_file)
     incrimentor +=divisor 
     
-    with open('new_file.json', 'w') as outfile:
-        json.dump(new_file, outfile)
     #incrimentor +=divisor 
 
 #print(new_file)
-#with open('new_file.json', 'w') as outfile:
- #   json.dump(new_file, outfile)
+with open('new_file.json', 'w') as outfile:
+   json.dump(new_file, outfile)
