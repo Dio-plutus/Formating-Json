4cc82fad21eb3072f285857a3a879c608eb4691d
Dio-plutus <67711256+Dio-plutus@users.noreply.github.com>
2021-05-19 22:30:37 +0100

    Add files via upload

---
+++ json/editjson.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import json
+
+with open("labels_to_name.json", "r") as read:
+    
+    #data = demjson.decode(read)
+    data=json.load(read)
+
+divisor = 100
+total = len(data)
+average = round(total/divisor)
+incrimentor = 0
+
+for i in range(0,average):
+    #print(f"{incrimentor+1} - {divisor*(i+1)}", data[f"{incrimentor}"])
+    new_file ={}
+    new_file[f"{incrimentor+1} - {divisor*(i+1)}"] = data[f"{incrimentor}"]
+    #new_file = incrimentor+1 - divisor*(i+1), data[incrimentor]
+    print(new_file)
+    incrimentor +=divisor 
+    
+    with open('new_file.json', 'w') as outfile:
+        json.dump(new_file, outfile)
+    #incrimentor +=divisor 
+
+#print(new_file)
+#with open('new_file.json', 'w') as outfile:
+ #   json.dump(new_file, outfile)
