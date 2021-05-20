2d177ad3cd680190fbf7cfa7d28f1b22d0550ab4
Dio-plutus <67711256+Dio-plutus@users.noreply.github.com>
2021-05-20 09:54:19 +0100

    Update editjson.py

--- json/editjson.py
+++ json/editjson.py
@@ -3,7 +3,6 @@ import json
 
 with open("labels_to_name.json", "r") as read:
     
-    #data = demjson.decode(read)
     data=json.load(read)
 
 divisor = 100
@@ -13,14 +12,8 @@ incrimentor = 0
 
 new_file ={}
 for i in range(0,average):
-    #print(f"{incrimentor+1} - {divisor*(i+1)}", data[f"{incrimentor}"])
     new_file[f"{incrimentor+1} - {divisor*(i+1)}"] = data[f"{incrimentor}"]
-    #new_file = incrimentor+1 - divisor*(i+1), data[incrimentor]
-    print(new_file)
     incrimentor +=divisor 
     
-    #incrimentor +=divisor 
-
-#print(new_file)
 with open('new_file.json', 'w') as outfile:
    json.dump(new_file, outfile)
